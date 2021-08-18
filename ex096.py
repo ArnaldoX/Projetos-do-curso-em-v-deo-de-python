60e286e6b9edb5ac797ab4799f790047024c37ca
ArnaldoX <arnaldoleal95@gmail.com>
2021-08-17 22:27:31 -0300

    Controle do tamanho de terrenos em python ex096.py

--- ex096.py
+++ ex096.py
@@ -1,1 +1,16 @@
-print('Arnaldo')
+#Criando um programa que tenha a função chamada área,
+# que receba as dimensões de um terreno retangular(largura, e comprimento), que mostre a área do terreno.
+
+def area(larg,comp):
+    a = larg * comp
+    print(f'A área do terreno é {larg} x {comp} é de {a}m²')
+
+
+print('='*30)
+print('\033[1;36mControle tamanho de terrenos')
+print('='*30)
+larg = float(input('Qual a largura do terreno?em m²: '))
+comp = float(input('Qual o comprimento do terreno?em m² '))
+area(larg,comp)
+
+
